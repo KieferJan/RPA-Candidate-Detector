31466b5b1131a089a5028a0bfe4a8849d3ae226e
KieferJan <jkiefer95@gmail.com>
2020-10-15 18:24:53 +0200

    load xes and parse activities with bert

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+import pandas as pd
+import bert_parser.main as bp
+from pm4py.objects.conversion.log import converter as log_converter
+from pm4py.objects.log.importer.xes import importer as xes_importer
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def import_xes():
+    pd.set_option('display.max_columns', None)
+    pd.options.display.width = None
+    log = xes_importer.apply('event logs/RequestForPayment.xes')
+    df = log_converter.apply(log, variant=log_converter.Variants.TO_DATA_FRAME)
+    return df[['org:resource', 'concept:name', 'time:timestamp', 'org:role', 'case:Rfp_id']].copy()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+def extract_activity_labels(df):
+    events = df['concept:name']
+    tagged_events = bp.main(events)
+    for key in tagged_events:
+        print(key, '->', tagged_events[key])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    df = import_xes()
+    extract_activity_labels(df)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
