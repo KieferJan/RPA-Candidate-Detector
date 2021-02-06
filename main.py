652f143508070149abf20f48d0cd229b1c479c0a
KieferJan <jkiefer95@gmail.com>
2021-02-06 13:13:32 +0100

    update classifier and feature set

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     full_activity_features_df = fe.extract_activity_features_full_log(default_df)
     #join the two data frames (full trace into unique activities)
     activity_df = fe.join_full_in_distinct(full_activity_features_df, distinct_activity_features_df)
-    print('----Features Extracted----')
+    print('----Features Extracted Directly From Event Log----')
     # # Workflow Step - Automation Indication BERT
     df = AutomationIndication.apply_bert(activity_df)
     df.to_csv('./extractedFeatures.csv', index=False, header=True)
